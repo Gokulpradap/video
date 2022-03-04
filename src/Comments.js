645af0bdc8220d621aa21f3b2b96482a70ff5db6
Gokulpradap <gokulmari96@gmail.com>
2022-03-03 20:56:33 -0800

    new file

---
+++ src/Comments.js
@@ -0,0 +1,91 @@
+import React from 'react'
+
+function Comments() {
+  return (  
+    <div className='commentLine'>
+        <h3>Comments</h3>
+        <h5>user 1</h5>
+        <p>so good video</p>
+        <h5>user 2</h5>
+        <p>i need one more video</p>
+        <h5>user 3</h5>
+        <p>super</p>
+        <h5>user 4</h5>
+        <p>ver nice</p>
+        <h5>user 5</h5>
+        <p>i love this video</p>
+        <h5>user 6</h5>
+        <p>very very nice</p>
+        <h5>user 7</h5>
+        <p>expect more video like this</p>
+        <h5>user 8</h5>
+        <p>you rock it bro.</p>
+        <h5>user 9</h5>
+        <p>thanks for the video</p>
+        <h5>user 10</h5>
+        <p>wow just awesome video</p>
+        <h5>user 11</h5>
+        <p>so good video</p>
+        <h5>user 12</h5>
+        <p>i need one more video</p>
+        <h5>user 13</h5>
+        <p>super</p>
+        <h5>user 14</h5>
+        <p>ver nice</p>
+        <h5>user 15</h5>
+        <p>i love this video</p>
+        <h5>user 16</h5>
+        <p>very very nice</p>
+        <h5>user 17</h5>
+        <p>expect more video like this</p>
+        <h5>user 18</h5>
+        <p>you rock it bro.</p>
+        <h5>user 19</h5>
+        <p>thanks for the video</p>
+        <h5>user 20</h5>
+        <p>wow just awesome video</p>
+        <h5>user 1</h5>
+        <p>so good video</p>
+        <h5>user 2</h5>
+        <p>i need one more video</p>
+        <h5>user 3</h5>
+        <p>super</p>
+        <h5>user 4</h5>
+        <p>ver nice</p>
+        <h5>user 5</h5>
+        <p>i love this video</p>
+        <h5>user 6</h5>
+        <p>very very nice</p>
+        <h5>user 7</h5>
+        <p>expect more video like this</p>
+        <h5>user 8</h5>
+        <p>you rock it bro.</p>
+        <h5>user 9</h5>
+        <p>thanks for the video</p>
+        <h5>user 10</h5>
+        <p>wow just awesome video</p>
+        <h5>user 11</h5>
+        <p>so good video</p>
+        <h5>user 12</h5>
+        <p>i need one more video</p>
+        <h5>user 13</h5>
+        <p>super</p>
+        <h5>user 14</h5>
+        <p>ver nice</p>
+        <h5>user 15</h5>
+        <p>i love this video</p>
+        <h5>user 16</h5>
+        <p>very very nice</p>
+        <h5>user 17</h5>
+        <p>expect more video like this</p>
+        <h5>user 18</h5>
+        <p>you rock it bro.</p>
+        <h5>user 19</h5>
+        <p>thanks for the video</p>
+        <h5>user 20</h5>
+        <p>wow just awesome video</p>
+    </div>
+  )
+}
+
+export default Comments
---
+++ src/Video.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Video() {
+  return (
+    <div className='video'>
+      <video width="100%" height="50%" controls
+        src="Smile_do_not_need_money_😊_Happy_Mood_tamil_Whatsapp_Status_💞_Life_Feel_Dialogue__Whatsapp_Status(720p).mp4" type="video/mp4">
+    </video>
+    <h2>Smile Please</h2>
+    <p>10,724 viewsJan 5, 2019</p>
+    </div>
+  )
+}
+
+export default Video
