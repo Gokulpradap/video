53d2095bec7656708766e0011fd40db57fce3c44
Gokulpradap <gokulmari96@gmail.com>
2022-03-05 02:00:03 -0800

    share to git

---
+++ src/Static.js
@@ -0,0 +1,324 @@
+export const CommentArr = [
+   
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    {
+        name : "user1",
+        comment: "so good video"
+    },
+    
+]
