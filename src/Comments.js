73bfe45b2f9ae417146b0e519fc5ad8c48209249
Gokulpradap <gokulmari96@gmail.com>
2022-03-05 07:19:27 -0800

    done

--- src/Comments.js
+++ src/Comments.js
@@ -1,18 +1,61 @@
-import React from 'react'
-import { CommentArr } from './Static'
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { CommentArr } from "./Static";
 
 function Comments() {
-  const updated = CommentArr.slice(0,20)
-  return (  
-    <div className='commentLine'>
-        {updated.map((comment) =>(
-          <>
+  const [limit, setLimit] = useState(20);
+  const [loading, setLoading] = useState(false);
+
+  const updated = CommentArr.slice(0, limit);
+
+  const observer = useRef();
+
+  useEffect(() => {
+    setLoading(false);
+  }, [limit]);
+
+  const lastCommnentObserver = useCallback(
+    (node) => {
+      console.log(node);
+      if (limit > CommentArr.length) return;
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+
+      observer.current = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting) {
+            setLoading(true);
+            setTimeout(() => {
+              setLimit((prev) => prev + 10);
+            }, 1500);
+          }
+        },
+        { threshold: 1 }
+      );
+
+      if (node) observer.current.observe(node);
+     
+    },
+    [loading]
+  );
+
+  return (
+    <div className="commentLine">
+      {updated.map((comment, idx) => (
+        <div key={idx} className="comment" ref={lastCommnentObserver}>
           <h5>{comment.name}</h5>
           <p>{comment.comment}</p>
-          </>
-        ) )}
+          <hr />
+        </div>
+      ))}
+
+      {loading && (
+        <div className="loading-cont">
+          <div className="spinner"></div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Comments
+export default Comments;
+
